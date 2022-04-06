549099ee58e0cce81c49e70a7f4f60afdc97cdd6
AsmaaFaried <asmaafaried54@gmail.com>
2022-04-06 17:44:25 +0200

    first commit

--- Modules/CheckModule.js
+++ Modules/CheckModule.js
@@ -1,9 +1,14 @@
-var name='',dataOfBirth=new Date(),errorDate="Please enter any year except 2020";
+var name='',dataOfBirth=new Date(),errorDate="Please enter valid data or any year except 2020";
 
 function UserDate(name,dataOfBirth){
-
+    if(name==='' || dataOfBirth===''){
+        console.error("Please send your name and age");
+    }
     if(dataOfBirth.getFullYear() =='2020'){
         console.error(errorDate);
+    }
+    if(name.length <3 || typeof name !=='string'){
+        console.error("Please enter valid name");
     }
     else{
         console.log('Your name is '+name,' and Your agen '+dataOfBirth);
